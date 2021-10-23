47c5b184450bdab06e6749257ca119e25db726a3
Fernando Patrick <78447989+FernandoPatrick@users.noreply.github.com>
2021-10-23 16:00:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: "FernandoPatrick",
+  youtube: "channel/UC7CQ7kXLizLlB23QasPi_KA",
+  facebook: "fernndopatrick",
+  instagram: "fernandoupatrick",
+  twitter: "Fernand34796788",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
+}
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.jason())
+    .then((data) => {
+      userName.texteContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      UserPhoto.src = data.avatar_url;
+      userLogin.textcontent = data.Login;
+    });
+}
+
+getGitHubProfileInfos();
